ba7eafbe80fbe0e6d6ded99f7a85d3978302def5
nico1988 <410827024@qq.com>
2019-11-04 14:45:00 +0800

    usereducer useeffect

--- src/component/App.js
+++ src/component/App.js
@@ -1,64 +1,80 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer, useEffect } from "react";
+
 import "../App.css";
 import Header from "./Header";
 import Movie from "./Movie";
+import spinner from "../assets/ajax-loader.gif";
 import Search from "./Search";
+import { initialState, reducer } from "../store/reducer";
+import axios from "axios";
 
-
-const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=4a3b711b"; // you should replace this with yours
-
+const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=85308f09";
 
 const App = () => {
-  const [loading, setLoading] = useState(true); // hooks
-  const [movies, setMovies] = useState([]); // hooks
-  const [errorMessage, setErrorMessage] = useState(null); // hooks
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  // If you’re familiar with React class lifecycle methods, you can think of useEffect Hook as componentDidMount, componentDidUpdate, and componentWillUnmount combined.
   useEffect(() => {
-    fetch(MOVIE_API_URL)
-      .then(response => response.json())
-      .then(jsonResponse => {
-        setMovies(jsonResponse.Search);
-        setLoading(false);
+    axios.get(MOVIE_API_URL).then(jsonResponse => {
+      dispatch({
+        type: "SEARCH_MOVIES_SUCCESS",
+        payload: jsonResponse.data.Search
       });
+    });
   }, []);
 
-  const search = searchValue => {
-    setLoading(true);
-    setErrorMessage(null);
-
-    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
-      .then(response => response.json())
-      .then(jsonResponse => {
-        if (jsonResponse.Response === "True") {
-          setMovies(jsonResponse.Search);
-          setLoading(false);
-        } else {
-          setErrorMessage(jsonResponse.Error);
-          setLoading(false);
-        }
-      });
+  // you can add this to the onClick listener of the Header component
+  const refreshPage = () => {
+    window.location.reload();
   };
-    
+
+  const search = searchValue => {
+    dispatch({
+      type: "SEARCH_MOVIES_REQUEST"
+    });
+
+    axios(`https://www.omdbapi.com/?s=${searchValue}&apikey=85308f09`).then(
+      jsonResponse => {
+        if (jsonResponse.data.Response === "True") {
+          dispatch({
+            type: "SEARCH_MOVIES_SUCCESS",
+            payload: jsonResponse.data.Search
+          });
+        } else {
+          dispatch({
+            type: "SEARCH_MOVIES_FAILURE",
+            error: jsonResponse.data.Error
+          });
+        }
+      }
+    );
+  };
+
+  const { movies, errorMessage, loading } = state;
+
+  const retrievedMovies =
+    loading && !errorMessage ? (
+      <img className="spinner" src={spinner} alt="Loading spinner" />
+    ) : errorMessage ? (
+      <div className="errorMessage">{errorMessage}</div>
+    ) : (
+      movies.map((movie, index) => (
+        <Movie key={`${index}-${movie.Title}`} movie={movie} />
+      ))
+    );
+
   return (
     <div className="App">
-      <Header text="React Movie Demo" />
-      <Search search={search} />
-      <p className="App-intro">Sharing a few of our favourite movies</p>
-      <div className="movies">
-        {loading && !errorMessage ? (
-         <span>loading...</span>
-         ) : errorMessage ? (
-          <div className="errorMessage">{errorMessage}</div>
-        ) : (
-          movies.map((movie, index) => (
-            <Movie key={`${index}-${movie.Title}`} movie={movie} />
-          ))
-        )}
+      <div className="m-container">
+        <Header text="HOOKED" />
+
+        <Search search={search} />
+
+        <p className="App-intro">Sharing a few of our favourite movies</p>
+
+        <div className="movies">{retrievedMovies}</div>
       </div>
     </div>
   );
 };
-
 
 export default App;
